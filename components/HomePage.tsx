003359e141fe545ea33eb3a6cad2c9e886f4f09b
uvacoder <uva22940coder@gmail.com>
2023-02-08 23:20:11 -0500

    Update HomePage.tsx

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -36,14 +36,14 @@ function HomePage(){
             <Grid.Col md={6}>
                 
                 <Card shadow="sm" p="lg" radius="md" style={{ backgroundColor: "#3b3b3b", color: "#fcf0f0" }}>
-
+                    <UnstyledButton onClick={ () => router.push(href: "/resources/portfolios")}>
                     <Group align="center">
                         <Avatar color="blue">
                             <IconBook2 size={24} />
                         </Avatar>
                         <Text size="lg" weight={600}>Ready to use</Text>
                     </Group>
-
+                    </UnstyledButton>
                     <Space h="lg" />
                     <Space h="lg" />
                     <Text size={16}>
